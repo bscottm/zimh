672272cebf20ab7f4be46a36ae7ff8309a1fabee
Perry E. Metzger <perry@piermont.com>
2026-06-02 12:18:04 -0400

    Update .gitignore and deps.sh
    Update the dependency helper to use current package names for PCRE2,
    pkgconf, FreeType, CMocka, and Ninja across the supported package
    managers. Expand the Arch Linux package list so it covers the expected
    build tools, SDL/font dependencies, compression, and network libraries.
    
    Also ignore a few local editor and build artifacts.
    
    This adopts the corresponding cleanup from B. Scott Michel's PR 12.

--- tools/ci/deps/deps.sh
+++ tools/ci/deps/deps.sh
@@ -2,84 +2,90 @@
 
 install_osx() {
     brew update
-    brew install pkg-config pcre libpng libedit sdl2 freetype2 sdl2_ttf \
-        vde cmake gnu-getopt coreutils zlib
+    brew install pkgconf pcre2 libpng libedit sdl2 freetype sdl2_ttf \
+        vde cmake zlib cmocka ninja
 }
 
 install_macports() {
-    sudo port install pkgconfig pcre libpng libedit libsdl2 freetype libsdl2_ttf \
-        vde2 cmake util-linux coreutils zlib
+    sudo port install pkgconf pcre2 libpng libedit libsdl2 freetype libsdl2_ttf \
+        vde2 cmake util-linux zlib cmocka ninja
 }
 
 install_arch_linux() {
+    sudo pacman -S --noconfirm base-devel git bison python
     sudo pacman -S --noconfirm pkgconf
-    sudo pacman -S --noconfirm pcre libpng libedit util-linux
+    sudo pacman -S --noconfirm pcre2 libpng libedit util-linux zlib
     sudo pacman -S --noconfirm mesa
     sudo pacman -S --noconfirm libsm
-    sudo pacman -S --noconfirm cmake
-
+    sudo pacman -S --noconfirm freetype2 sdl2-compat sdl2_ttf
+    sudo pacman -S --noconfirm libpcap libslirp vde2
+    sudo pacman -S --noconfirm cmake cmocka ninja
 }
 
 install_linux() {
     sudo apt-get update -yqqm
     sudo apt-get install -ym pkg-config
-    sudo apt-get install -ym libpcre3-dev libpng-dev libedit-dev uuid-dev
+    sudo apt-get install -ym libpcre2-dev libpng-dev libedit-dev uuid-dev
     sudo apt-get install -ym libegl1-mesa-dev libgles2-mesa-dev
     sudo apt-get install -ym libsdl2-dev libfreetype6-dev libsdl2-ttf-dev
     sudo apt-get install -ym libpcap-dev libvdeplug-dev
-    sudo apt-get install -ym cmake cmake-data ninja-build
+    sudo apt-get install -ym cmake cmake-data ninja-build libcmocka-dev
 }
 
 install_mingw32() {
     ## Doesn't have libpcap or cmake's extra modules. Not that this
     ## makes much of a difference.
     pacman -S --needed mingw-w64-i686-ninja \
-	mingw-w64-i686-cmake \
+        mingw-w64-i686-cmake \
         mingw-w64-i686-gcc \
 	mingw-w64-i686-make \
-        mingw-w64-i686-pcre \
+        mingw-w64-i686-pcre2 \
 	mingw-w64-i686-freetype \
         mingw-w64-i686-SDL2 \
-	mingw-w64-i686-SDL2_ttf
+	mingw-w64-i686-SDL2_ttf \
+	mingw-w64-i686-cmocka
 }
 
 install_mingw64() {
     pacman -S --needed mingw-w64-x86_64-ninja \
 	mingw-w64-x86_64-cmake \
-	mingw-w64-x86_64-extra-cmake-modules \
+        mingw-w64-x86_64-extra-cmake-modules \
         mingw-w64-x86_64-gcc \
 	mingw-w64-x86_64-make \
-        mingw-w64-x86_64-pcre \
+        mingw-w64-x86_64-pcre2 \
 	mingw-w64-x86_64-freetype \
         mingw-w64-x86_64-SDL2 \
 	mingw-w64-x86_64-SDL2_ttf \
-	mingw-w64-x86_64-libpcap
+	mingw-w64-x86_64-libpcap \
+	mingw-w64-x86_64-cmocka
 }
 
 install_ucrt64() {
     pacman -S --needed mingw-w64-ucrt-x86_64-ninja \
 	mingw-w64-ucrt-x86_64-cmake \
-	mingw-w64-ucrt-x86_64-extra-cmake-modules \
+        mingw-w64-ucrt-x86_64-extra-cmake-modules \
         mingw-w64-ucrt-x86_64-gcc \
 	mingw-w64-ucrt-x86_64-make \
-        mingw-w64-ucrt-x86_64-pcre \
+        mingw-w64-ucrt-x86_64-pcre2 \
 	mingw-w64-ucrt-x86_64-freetype \
         mingw-w64-ucrt-x86_64-SDL2 \
 	mingw-w64-ucrt-x86_64-SDL2_ttf \
-	mingw-w64-ucrt-x86_64-libpcap
+	mingw-w64-ucrt-x86_64-libpcap \
+	mingw-w64-ucrt-x86_64-cmocka
 }
 
 install_clang64() {
     pacman -S --needed mingw-w64-clang-x86_64-ninja \
-	mingw-w64-clang-x86_64-cmake \
+        mingw-w64-clang-x86_64-cmake \
 	mingw-w64-clang-x86_64-extra-cmake-modules \
         mingw-w64-clang-x86_64-clang \
 	mingw-w64-clang-x86_64-make \
-        mingw-w64-clang-x86_64-pcre \
+        mingw-w64-clang-x86_64-pcre2 \
 	mingw-w64-clang-x86_64-freetype \
         mingw-w64-clang-x86_64-SDL2 \
 	mingw-w64-clang-x86_64-SDL2_ttf \
-	mingw-w64-clang-x86_64-libpcap
+	mingw-w64-clang-x86_64-libpcap \
+	mingw-w64-clang-x86_64-cmocka
 }
 
 
